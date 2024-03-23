cfcde6e6ef450e273a09fc3b40496f3d6a19a465
mohamedelbendary <mohamed.elbendary@mobidevlabs.com>
2024-03-23 23:19:12 +0200

    true fade in slide animation with animate_do package

--- lib/implicit_examples/animated_color_pallete.dart
+++ lib/implicit_examples/animated_color_pallete.dart
@@ -1,3 +1,4 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'dart:math';
 
@@ -10,6 +11,8 @@ class AnimatedColorPalette extends StatefulWidget {
 
 class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
   List<Color> currentPalette = generateRandomPalette();
+
+  bool animatedContainer = false;
 
   static List<Color> generateRandomPalette() {
     final random = Random();
@@ -30,30 +33,54 @@ class _AnimatedColorPaletteState extends State<AnimatedColorPalette> {
     });
   }
 
+  void startAnimation() {
+    setState(() {
+      animatedContainer = !animatedContainer;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Color Palette Generator'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            for (Color color in currentPalette)
-              Container(
-                width: 100,
-                height: 100,
-                color: color,
-                margin: const EdgeInsets.all(8),
-              ),
-            ElevatedButton(
-              onPressed: regeneratePalette,
-              child: const Text('Generate New Palette'),
-            ),
-          ],
+        appBar: AppBar(
+          title: const Text('Color Palette Generator'),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+             mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SlideInUp(
+                from: 100,
+                duration: const Duration(milliseconds: 800),
+                curve: Curves.linear,
+                animate: animatedContainer,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                  margin: const EdgeInsets.all(8),
+                ),
+              ),
+              FadeInUp(
+                from: 400,
+                duration: const Duration(milliseconds: 800),
+                curve: Curves.linear,
+                animate: animatedContainer,
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  color: Colors.green,
+                  margin: const EdgeInsets.all(8),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: startAnimation,
+                child: const Text('Start animation'),
+              ),
+            ],
+          ),
+        )
+
+        );
   }
 }
