66d956752719e5017214b2ec21e365bff15a165a
mohamedelbendary <mohamed.elbendary@mobidevlabs.com>
2024-04-28 18:22:39 +0300

    Add Page transition with animation

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:animation_starter_code/explicit_examples/list_animation.dart';
 import 'package:animation_starter_code/explicit_examples/loading_animation.dart';
 import 'package:animation_starter_code/explicit_examples/login_animation.dart';
 import 'package:animation_starter_code/implicit_examples/animated_tween_animation_builder_example.dart';
+import 'package:animation_starter_code/page_route_builder_animation/splash_animation.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -20,10 +21,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RadialProgressAnimation(
-        color: Colors.deepPurple,
-        progress: 0.70,
-      ),
+      home: const SplashAnimation(),
     );
   }
 }
--- lib/page_route_builder_animation/splash_animation.dart
+++ lib/page_route_builder_animation/splash_animation.dart
@@ -1,23 +1,68 @@
 import 'package:flutter/material.dart';
 
-class SplashAnimation extends StatelessWidget {
+class SplashAnimation extends StatefulWidget {
   const SplashAnimation({super.key});
+
+  @override
+  State<SplashAnimation> createState() => _SplashAnimationState();
+}
+
+class _SplashAnimationState extends State<SplashAnimation>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> scaleAnimation;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+        vsync: this, duration: const Duration(milliseconds: 500));
+    scaleAnimation = Tween<double>(begin: 1.0, end: 10.0).animate(controller);
+    controller.addListener(() {
+      if (controller.isCompleted) {
+        Navigator.of(context).push(
+          PageRouteBuilder(
+              pageBuilder: (context, animation, secondaryAnimation) {
+            return const Destination();
+          }, transitionsBuilder:
+                  (context, animation, secondaryAnimation, child) {
+            // final tweenPosition =
+            //     Tween<Offset>(begin: const Offset(0, -1), end: Offset.zero)
+            //         .animate(animation);
+            return FadeTransition(
+              opacity: animation,
+              child: child,
+            );
+          }),
+        );
+
+        Future.delayed(
+          const Duration(milliseconds: 500),
+          () {
+            controller.reset();
+          },
+        );
+      }
+    });
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Container(
-          width: 100,
-          height: 100,
-          decoration: const BoxDecoration(
-            color: Colors.blue,
-            shape: BoxShape.circle,
-          ),
-          child: Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Colors.blue,
+        child: GestureDetector(
+          onTap: () {
+            controller.forward();
+          },
+          child: ScaleTransition(
+            scale: scaleAnimation,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Colors.deepPurple,
+                shape: BoxShape.circle,
+              ),
             ),
           ),
         ),
@@ -32,9 +77,16 @@ class Destination extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
+      // backgroundColor: Colors.deepPurpleAccent,
       appBar: AppBar(
         title: const Text('Go Back'),
+      ),
+      body: const Center(
+        child: Text(
+          "Welcome to Destination",
+          style: TextStyle(
+              color: Colors.black, fontSize: 25, fontWeight: FontWeight.w600),
+        ),
       ),
     );
   }
